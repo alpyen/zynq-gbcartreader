3e344abc18527c2858285e80ceceea206b299e48
alpyen <53614168+alpyen@users.noreply.github.com>
2025-12-05 15:19:10 +0100

    src: made uart_readline more concise

--- src/standalone.cpp
+++ src/standalone.cpp
@@ -3,6 +3,8 @@
 #include <xuartps.h>
 #include "common.h"
 
+/* NOTE: This function fills up the buffer and overwrites only the last character
+         if more arrive than the buffer can handle. It breaks upon receciving '\r'. */
 void uart_readline(char* buffer, uint8_t buffer_size)
 {
     uint8_t num_received = 0;
@@ -10,20 +12,22 @@ void uart_readline(char* buffer, uint8_t buffer_size)
     while (true)
     {
         char received = XUartPs_RecvByte(STDOUT_BASEADDRESS);
+        num_received++;
 
         // Transform to lower case for strcmp
         if (received >= 'A' && received <= 'Z')
             received += 'a' - 'A';
 
-        buffer[num_received] = received;
-        num_received++;
+        buffer[num_received - 1] = received;
 
-        if (num_received > buffer_size - 1)
-            num_received--;
-
+        // Command complete?
         if (received == '\r') break;
+
+        // Decrement buffer pointer to point to last element again.
+        if (num_received == buffer_size)
+            num_received--;
     }
 
-    // Overwrite the \r line break with null-terminator
+    // Overwrite the \r line break with null-terminator for strcmp.
     buffer[num_received - 1] = '\0';
 }
